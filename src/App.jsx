62bdd3d17e1663285147d96aa46da9345cf9f1be
BegoCaZa <begoxcz@hotmail.com>
2025-05-08 11:53:23 +0200

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import Button from './components/button/Button';
+import {useState} from `react`;
+
+import { useState } from 'react';
+import { USERS } from './constants/users-info';
 
 const App = () => {
+  const [counter,setCounter]=useState(0);
   return (
-    <>
-      <h1>React</h1> <Button />
-    </>
+    <div>
+      <img src={USERS[0].profileImage} />
+      <h2>Name:{USERS[0].name}</h2>
+      <p>UserName:{USERS[0].username}</p>
+      <p>Email:{USERS[0].email}</p>
+      <div>
+        <button onClick={()=>nextUser(counter,setCounter)}></button>
+      </div>
+    </div>
   );
 };
+
+
 
 export default App;
